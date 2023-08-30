9341f7bd086fad7ad074899dd7d8d56564c366b0
thesahibnanda <essenbeats@gmail.com>
2023-08-30 11:42:32 +0530

    fix: nested inputs in input layer

--- Layers/Input_Layer.cpp
+++ Layers/Input_Layer.cpp
@@ -1,58 +1,88 @@
-// Headers (Headers Are Files That Can Be Imported)
 #include <iostream>
 #include <vector>
 
+using NestedVector = std::vector<std::vector<double>>; // For 2D inputs
 
-class InputLayer { // Class Definition
-
-    // Private Members Of Class
+class InputLayer {
     private:
-        std::vector<double> inputs;  // Vector to hold the input neurons
-        int numInputs;  // Number of input neurons
-    // Public Members
-    public:
-        // Constructor
-        InputLayer(int numInputs) : numInputs(numInputs) {
-            inputs.resize(numInputs, 0.0);  // Initialize with zeros
+        NestedVector inputs;  // For 2D inputs
+        std::vector<double> inputs_1D; // For 1D inputs
+        int numInputs;
+        bool is1D; // A flag to indicate if the input is 1D or not
+
+        bool checkNestedVectorSize(const NestedVector& vec, int expectedSize) {
+            if (vec.size() != expectedSize) return false;
+            for (const auto& subVec : vec) {
+                if (subVec.size() != expectedSize) return false;
+            }
+            return true;
         }
 
-        // Method to set the input values
+    public:
+        InputLayer(int numInputs) : numInputs(numInputs), is1D(true) {
+            inputs_1D.resize(numInputs, 0.0);
+        }
+
+        // For 1D input
         void setInputs(const std::vector<double>& newInputs) {
             if (newInputs.size() != numInputs) {
                 std::cerr << "Error: Mismatch in number of input neurons." << std::endl;
                 return;
             }
-            inputs = newInputs;
+            inputs_1D = newInputs;
+            is1D = true; // set the flag to indicate it's 1D input
         }
 
-        // Method to get the input values
-        std::vector<double> getInputs() const {
-            return inputs;
-        }
-
-        // Method to print the input values
-        void printInputs() const {
-            std::cout << "Inputs: ";
-            for (const auto& input : inputs) {
-                std::cout << input << " ";
+        // For 2D input
+        void setInputs(const NestedVector& newInputs) {
+            if (!checkNestedVectorSize(newInputs, numInputs)) {
+                std::cerr << "Error: Mismatch in number of input neurons." << std::endl;
+                return;
             }
-            std::cout << std::endl;
+            inputs = newInputs;
+            is1D = false; // reset the flag to indicate it's not 1D input
+        }
+
+        void printInputs() const {
+            if (is1D) {
+                std::cout << "1D Inputs: ";
+                for (const auto& input : inputs_1D) {
+                    std::cout << input << " ";
+                }
+                std::cout << std::endl;
+            } else {
+                std::cout << "2D Inputs: " << std::endl;
+                for (const auto& row : inputs) {
+                    for (const auto& input : row) {
+                        std::cout << input << " ";
+                    }
+                    std::cout << std::endl;
+                }
+            }
         }
 };
 
-
-// Test the InputLayer class
 int main() {
-    // Create an InputLayer with 5 neurons
-    InputLayer inputLayer(5);
+    InputLayer inputLayer1(5);
 
-    // Set some sample inputs
-    std::vector<double> sampleInputs = {1.0, 2.0, 3.0, 4.0, 5.0};
-    inputLayer.setInputs(sampleInputs);
+    // For 1D input
+    std::vector<double> sampleInputs_1D = {1.0, 2.0, 3.0, 4.0, 5.0};
+    inputLayer1.setInputs(sampleInputs_1D);
+    inputLayer1.printInputs();
 
-    // Get and print the inputs
-    inputLayer.printInputs();
+    // For 2D input
 
-    // Return Statement
+    InputLayer inputLayer2(5);
+    
+    NestedVector sampleInputs_2D = {
+        {1.0, 2.0, 3.0, 4.0, 5.0},
+        {5.0, 4.0, 3.0, 2.0, 1.0},
+        {1.1, 2.1, 3.1, 4.1, 5.1},
+        {5.1, 4.1, 3.1, 2.1, 1.1},
+        {0.0, 0.0, 0.0, 0.0, 0.0}
+    };
+    inputLayer2.setInputs(sampleInputs_2D);
+    inputLayer2.printInputs();
+
     return 0;
 }
