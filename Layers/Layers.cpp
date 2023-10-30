61264b9743080010730872a52ad3eeb50de807e4
thesahibnanda <essenbeats@gmail.com>
2023-10-30 09:48:05 +0530

    refractor: Corrected Code DT, Comments Optimized & Added Test Case

--- Layers/Layers.cpp
+++ Layers/Layers.cpp
@@ -18,8 +18,9 @@
 using ActFunc = std::function<double(double)>;
 using NestedVector = std::vector<std::vector<double>>;
 
+//**ANANYA BUNDELA** Added Softmax In enum Activation
 enum Activation {
-    RELU, LEAKY_RELU, PARAMETRIC_RELU, SWISH, EXPONENTIAL, TANH, LINEAR, GELU, SIGMOID, NONE, SQUARE, SQUARE_ROOT, CUBIC,SOFTMAX
+    RELU, LEAKY_RELU, PARAMETRIC_RELU, SWISH, EXPONENTIAL, TANH, LINEAR, GELU, SIGMOID, NONE, SQUARE, SQUARE_ROOT, CUBIC, SOFTMAX
 };
 
 double relu(double x) {
@@ -68,9 +69,13 @@ double square_root(double x) {
 double cubic(double x) {
     return x * x * x;
 }
-double softmax(double x){//*ANANYA*-Softmax
-return std::exp(x) / (1 + std::exp(x));
+
+// **ANANYA BUNDELA** Softmax
+//START
+double softmax(double x) {
+    return std::exp(x) / (1.0 + std::exp(x));
 }
+//END
 
 class Layer {
 public:
@@ -195,7 +200,7 @@ public:
         case SQUARE: activation = square; break; // Add SQUARE activation
         case SQUARE_ROOT: activation = square_root; break; // Add SQUARE_ROOT activation
         case CUBIC: activation = cubic; break; // Add CUBIC activation
-        case SOFTMAX:activation= softmax;break;//*ANANYA*-Softmax
+        case SOFTMAX:activation= softmax;break;//**ANANYA BUNDELA** Added Softmax
         default: throw std::invalid_argument("Unsupported activation function");
         }
     }
@@ -263,9 +268,9 @@ int main() {
     model.add(new DenseLayer(5, 2, SIGMOID, DenseLayer::HE));
     model.add(new DenseLayer(2, 10, SIGMOID, DenseLayer::HE_UNIFORM));
     model.add(new DenseLayer(10, 2, SQUARE, DenseLayer::LECUN_NORMAL));
-    model.add(new DenseLayer(2, 1, CUBIC, DenseLayer::LECUN_UNIFORM));
-    model.add(new DenseLayer(1, 1, SQUARE_ROOT, DenseLayer::XAVIER));
-    model.add(new DenseLayer(1, 1, SIGMOID, DenseLayer::RANDOM));
+    model.add(new DenseLayer(2, 11, CUBIC, DenseLayer::LECUN_UNIFORM));
+    model.add(new DenseLayer(11, 12, SQUARE_ROOT, DenseLayer::XAVIER));
+    model.add(new DenseLayer(12, 1, SOFTMAX, DenseLayer::RANDOM)); // Softmax
 // Manual initialization
 
     NestedVector x_test = sampleInputs;
